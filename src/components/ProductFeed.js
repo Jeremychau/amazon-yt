59710d176f820067f2abe97394babeae86b08da5
Chau Ka Leung <lacusleung012@gmail.com>
2021-10-16 23:08:16 +0800

    init commit

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,54 @@
+import Product from "./Product"
+
+function ProductFeed({ products }) {
+    return (
+        <div className="grid grid-flow-row-dense md:grid-flow-col-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            {products.slice(0, 4).map( ({id, title, price, description, category, image}, index) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    category={category}
+                    description={description}
+                    image={image}
+                />
+            ) )}
+
+            <img
+                className="col-span-full my-auto ml-2"
+                src="https://links.papareact.com/dyz"
+                alt=""
+            />
+
+
+            <div className="md:col-span-2">
+                {products.slice(4, 5).map( ({id, title, price, description, category, image}, index) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        category={category}
+                        description={description}
+                        image={image}
+                    />
+                ) )}
+            </div>
+
+            {products.slice(5, products.length).map( ({id, title, price, description, category, image}, index) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    category={category}
+                    description={description}
+                    image={image}
+                />
+            ) )}
+        </div>
+    )
+}
+
+export default ProductFeed
